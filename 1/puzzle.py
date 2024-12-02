4509f9b8f2d61c7d9306cad8ecac433cdc5d0196
pointatnick <nicholashma@gmail.com>
2024-12-01 17:39:12 -0800

    Solve Day 1 Part 2

--- 1/puzzle.py
+++ 1/puzzle.py
@@ -8,22 +8,58 @@ def unzip(tuples: list[tuple[int, int]]) -> tuple[list[int], list[int]]:
     return list(x), list(y)
 
 
-def measure_lists(first: list[int], second: list[int]) -> int:
+def measure_distance(first: list[int], second: list[int]) -> int:
+    """Measures the distance between two lists, both sorted in ascending order.
+    Distance is the absolute value between a number in one list and its
+    corresponding number in the same index in the other list.
+    """
     total = 0
     for x, y in zip(first, second):
         total += abs(x - y)
     return total
 
 
-def solve():
+def get_appearances(list: list[int]) -> dict[int, int]:
+    apps = {}
+    for x in list:
+        if x in apps:
+            apps[x] += 1
+        else:
+            apps[x] = 1
+    return apps
+
+
+def measure_similarity(list: list[int], appearances: dict[int, int]) -> int:
+    """Measures the similarity of a list. Similarity is the product of a number
+    in the first list multiplied by the number of times it appears in the
+    appearance dict.
+    """
+    total = 0
+    for x in list:
+        total += x * appearances.get(x, 0)
+    return total
+
+
+def solve_part_one():
     with open("input.txt", encoding="utf-8") as f:
         pairs = [parse(line) for line in f.readlines()]
 
     x, y = unzip(pairs)
     x.sort()
     y.sort()
-    print(f"Distance is {measure_lists(x, y)}")
+    print(f"Distance is {measure_distance(x, y)}")
+
+
+def solve_part_two():
+    with open("input.txt", encoding="utf-8") as f:
+        pairs = [parse(line) for line in f.readlines()]
+
+    x, y = unzip(pairs)
+    x.sort()
+    apps = get_appearances(y)
+    print(f"Similarity score is {measure_similarity(x, apps)}")
 
 
 if __name__ == "__main__":
-    solve()
+    solve_part_one()
+    solve_part_two()
--- 1/puzzle_test.py
+++ 1/puzzle_test.py
@@ -9,5 +9,24 @@ def test_it_unzips_tuples_into_two_lists():
     assert puzzle.unzip([(1, 4), (2, 5), (3, 6)]) == ([1, 2, 3], [4, 5, 6])
 
 
-def test_it_compares_numbers_and_sums_distance():
-    assert puzzle.measure_lists([1, 2, 3, 3, 3, 4], [3, 3, 3, 4, 5, 9]) == 11
+def test_it_measures_distance():
+    assert puzzle.measure_distance([1, 2, 3, 3, 3, 4], [3, 3, 3, 4, 5, 9]) == 11
+
+
+def test_it_records_appearances():
+    assert puzzle.get_appearances([3, 3, 3, 4, 5, 9]) == {
+        3: 3,
+        4: 1,
+        5: 1,
+        9: 1,
+    }
+
+
+def test_it_measures_similarity():
+    assert (
+        puzzle.measure_similarity(
+            [1, 2, 3, 3, 3, 4],
+            {3: 3, 4: 1, 5: 1, 9: 1},
+        )
+        == 31
+    )
